89fd54ef674fb90341166b0095930f0d29609647
NirobNabil <thetwin11100@gmail.com>
2018-09-01 01:29:40 +0600

    Update whythough.cpp
    added some comments

--- conditionals/whythough.cpp
+++ conditionals/whythough.cpp
@@ -26,13 +26,25 @@ int main()
     bool divisible_by_5 = (number % 5 == 0);
 
     bool divisible_by_3_and_5 = divisible_by_3 && divisible_by_5;
-
+    
+    // '||' is a logical OR operator. it returns true if any of the expressions at its right side and
+    // left side is true(here anything other that 0 means true)
     bool divisible_by_3_or_5 = divisible_by_3 || divisible_by_5;
 
+    // '!' is also a logical operator. its the logical NOT operator. basically it just reverses the value
+    // at its right side
     bool not_divisible_by_5 = !divisible_by_5;
 
     // more complex
+    // '&&' is a logical OR operator. it returns true if both expressions at its right side and
+    // left side is true(here anything other that 0 means true)
     bool not_divisible_by_3_nor_5 = !(divisible_by_3 && divisible_by_5);
+    
+    cout << divisible_by_3 << ' ' 
+         << divisible_by_5 << ' '
+         << divisible_by_3_and_5 << ' '
+         << not_divisible_by_5 << ' '
+         << not_divisible_by_3_nor_5 << '\n';
 
     return 0;
 }
