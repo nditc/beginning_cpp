72f1271a9820b61f3f01270de8f9828855ea5b47
NirobNabil <thetwin11100@gmail.com>
2018-09-01 02:18:07 +0600

    comments again >:(

--- hello/smallest_program_of_universe.cpp
+++ hello/smallest_program_of_universe.cpp
@@ -1,5 +1,23 @@
 
 int main() {
-
+    // here we are basically declaring a fucntion of int type just like any ordinary function
+    // but the special thing about main is when you run the program, the first thing that the OS
+    // dies is, the OS searches for a function named 'main' and executes that
+    
+    // we will go in depths with functions later. for now just know that functions are just a chunk of
+    // code and you can give that chunk of code an interesting name :3
+    
+    /*
+    .. statements ..
+    .. expression ..
+    */
+    
+    // '//' represents a one line comment. when you execute a program everything after the '//' in a
+    // single line will not be read by the compiler. 
+    
+    /* 
+       "/* ... */" is a multi line comment. the compiler will everything between '/*' and '*/'
+    */
+    
     return 0;
 }
